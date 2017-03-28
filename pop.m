5860e9f3def5f38f987c2c6d25fea7d247ac211d
Peter Reintjes <peter.reintjes@ncmls.org>
2017-03-28 13:24:05 -0400

    doc update

---
+++ pop.m
@@ -0,0 +1,38 @@
+function xdot = f(x,t)
+# SNR = [-10:5:30]; %in Db
+  snr = 130;
+
+  Dc = 1.0;
+  h0 = 10^9;
+  p = 0.4; # Population Density of Ho
+  k1 = 0.14;
+  k2 = awgn(0.4,snr,'measured');
+  k3 = 6.1;
+  k4 = 0.2;
+  kg = 0.5;
+
+  inhibit = 0.9;
+  kgp = kg*inhibit;
+  kgpp = kgp*inhibit;
+  kgppp = kgpp*inhibit;
+% Each successive growth rate (slightly) inhibited
+
+xdot(1) = Dc*h0 - Dc*x(1) - k1*p*x(1) + kg*x(1);
+
+
+xdot(2) = -Dc*x(2) + k1*x(1) + kgp*x(2) - k2*x(2);
+
+
+xdot(3) = -Dc*x(3) + kgpp*x(3) + k2*x(2) - k4*x(3);
+
+
+xdot(4) = -Dc*x(4) + kgppp*x(4) + k4*x(3);
+
+
+xdot(5) = -Dc*x(5) - k1*p*x(1) + k3*x(3);
+
+
+endfunction
+# Initial conditions x(1)=1 and x(2)=2 on [0,50] with 200 points:
+x = lsode("f", [1;0;0;0;0], (t = linspace (0,50,200)' ));
+plot(t,x);
