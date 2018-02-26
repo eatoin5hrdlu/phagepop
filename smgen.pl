5e79297f5534d34bb9d3d61e2fb941791b67dc2f
Peter Reintjes <8wan5hrdlu@gmail.com>
2018-02-25 20:32:02 -0500

    generating Python and Octave from smgen.pl

--- smgen.pl
+++ smgen.pl
@@ -1,56 +1,38 @@
 #!/usr/bin/swipl -s
-% Generate octave/python(odespy) solver for equations in file "eqn"
+% Generate octave/python(python) solver for equations in file "eqn"
 % ./smgen.pl eqn
 
 :- use_module(library(lists), [ nth0/3 ]).
 
 suffix(octave, '.m').
-suffix(odespy, '.py').
+suffix(matlab, '.m').
+suffix(python, '.py').
 
 % Compute +- parameter values for range of solutions
 deltaValue(octave, Sign, VN:PC, ['l',VN,' = delta(',VN,', ',Sign,PC,'*pm);']).
-deltaValue(odespy, Sign, VN:PC, ['\tl',VN,' = delta(',VN,', ',Sign,PC,'*pm);']).
+deltaValue(python, Sign, VN:PC, ['\tl',VN,' = delta(',VN,', ',Sign,PC,'*pm);']).
 
-% Code Wrapper Fragments:  Pre [Assignments] Mid [Equations] Post
-template(octave, Name, ['function xdot = ', Name, '(x,t)
-  global kg ad ec pp fr h0 vol;
-  pm = 1;'],
- [ 'inhibit = 1.1;
-  kgp = lkg*inhibit;
-  kgpp = kgp*inhibit;','\n'],
- ['endfunction','\n']).
-
-template(odespy, Name, ['def ',Name,
- '(x,t)\n\tglobal kg ad ec pp fr h0 vol;\n\tpm = 1;\n'],
- [ '\tinhibit = 1.1;\n\tkgp = lkg*inhibit;\n\tkgpp = kgp*inhibit;\n'],
- ['\n']).
-
-gencode(Language, Name, Spec, Sign,
-[ Pre, '\n',
-  Assignments, '\n',
-  Mid, '\n',
-  Equations,'\n',
-  Post ]) :-
-    template(Language, Name, Pre, Mid, Post),
-    maplist(deltaValue(Language, Sign), Spec, Assignments),
-    diffeq(Language, Equations).
+gencode(Language, Name, _Spec, _Sign) -->
+    declaration(Language, Name),
+    assignments(Language),
+    diffeq(Language).
 
 genmodels(Language) :-
     Spec = [kg:0.02, ad:0.1, ec:0.1, pp:0.05],
     writecode(Language, Spec, f2, ''),
     writecode(Language, Spec, g2, '-').
 
-flat_format([])    --> [].
-flat_format([H|T]) --> flat_format(H), flat_format(T).
-flat_format(octave(A))       --> !, [A, ';\n'].
-flat_format(odespy(Tabs, A)) --> !, tabs(Tabs), [A, '\n'].
+flat_format([])        --> [].
+flat_format([H|T])     --> flat_format(H), flat_format(T).
+flat_format(octave(A)) --> !, [A, ';'], newline.
+flat_format(python(Tabs, A)) --> !, tabs(Tabs), [A], newline.
 flat_format(A) --> [A].
 
 tabs(N) --> { N>0, !, NN is N-1}, ['\t'], tabs(NN).
 tabs(0) --> [].
 
 writecode(Language, Spec, Name, Sign) :-
-    gencode(Language, Name, Spec, Sign, Code),
+    gencode(Language, Name, Spec, Sign, Code, []),
     suffix(Language, Suffix),
     concat_atom([Name, Suffix],Filename),
     tell(Filename),
@@ -58,25 +40,53 @@ writecode(Language, Spec, Name, Sign) :-
     maplist(write, FlatCode),
     told.
 
-diffeq(octave, ODEs) :-
-    odes(Stmts),
-    findall(['xdot(',I,')= ', octave(S)], nth0(I,Stmts,_:S), ODEs).
+% ASSIGNMENT STATEMENT SYNTAX
+assignments(Language) --> { preamble(Pre) }, stmt(Pre,Language).
 
-diffeq(odespy, ['\treturn [\n',ODEs,']\n']) :-
-    odes(Stmts),
-    findall([S,',\n'], member(_:S,Stmts), ODEs).
+stmt([],    _)      --> !.
+stmt([H|T], octave) --> [ octave(H) ], stmt(T, octave).
+stmt([H|T], python) --> [ python(1, H) ], stmt(T, python).
 
-:- ( current_prolog_flag(argv,[A|_])
-    -> consult(A)
-    ; assert(odes([
-	       host0:'h0*fr*vol/60 +(log(2)/lkg)*x(1)-fr*x(1)/60-(lad/vol)*x(4)*x(1)',
-	       hosta:'(lad/vol)*x(4)*x(1)+(log(2)/kgp)*x(2)-fr*x(2)/60 - x(2)/lec',
-	       hostp:'x(2)/lec   + (log(2)/kgpp)*x(3)  - fr*x(3)/60',
-	       phage:'lpp*x(3)/60                - fr*x(4)/60',
-	       phagee:'lpfraction*x(4)/60                - fr*x(5)/60']))
-   ),
-   genmodels(octave),
-   genmodels(odespy),
-   halt.
+# DIFFERENTIAL EQUATION SYNTAX
+diffeq(octave) --> diffeq(matlab).
+
+diffeq(matlab) -->  { odes(Stmts),
+		      findall(['xdot(',I,')= ', octave(S)], nth0(I,Stmts,_=S), ODEs) },
+		    ODEs.
+
+diffeq(python) --> ['\t', return, '[' ], newline,
+		   { odes(Stmts),
+		     findall([S,',\n'], member(_=S,Stmts), ODEs) },
+		   ODEs, [']'], newline.
+
+run :- current_prolog_flag(argv,[A|_]),
+       consult(A),
+       genmodels(octave),
+       genmodels(python).
+
     
+freevars([],_)  :- !,fail.
+freevars(N,_)   :- number(N),!,fail.
+freevars(A,A)   :- atomic(A),!.
+freevars(Term,V):- Term =.. [_|Args], member(A,Args), freevars(A,V).
+    
+declaration(Language, Name) -->
+    { preamble(P),
+      setof(V,freevars(P,V),Vs) }, 
+    declare(Language, Name, Vs).
+    
+newline --> ['\n'].
+
+declare(octave, Name, List) --> declare(matlab, Name, List).
+
+declare(matlab, Name, List) --> ['function xdot = ', Name, '('], xandt, newline,
+                                [global, ' '], dvarlist(matlab, List), [';'], newline.
+
+declare(python, Name, List) --> [def, ' ', Name, '('], dvarlist(python, List), xandt, [':'], newline.
+
+xandt --> ['x', ',', 't', ')'].
+
+dvarlist(_,           []) --> [].
+dvarlist(matlab,     [H]) --> [H], !.
+dvarlist(Language, [H|T]) --> [H, ', '], dvarlist(Language, T).
 
